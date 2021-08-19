e45169230fd33747050e6c58b37ac94302ac64f3
klemie <lemieuxkristopher@gmail.com>
2021-08-18 17:59:39 -0700

    Set up ionic app

---
+++ workout-planner/src/pages/Workouts.tsx
@@ -0,0 +1,15 @@
+import { IonContent, IonPage, IonTitle } from '@ionic/react';
+import './Workouts.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonTitle className="workoutTitle">Work Outs</IonTitle>
+      <IonContent fullscreen>
+       
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
